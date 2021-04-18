0a21b945e28ffce4c2f038241ff8a2606f038ede
endo_wataru <greenstar284@gmail.com>
2021-04-18 16:22:04 +0900

    web.phpを編集

--- Desktop/techboost/mynews/routes/web.php
+++ Desktop/techboost/mynews/routes/web.php
@@ -19,3 +19,9 @@ Route::group(['prefix' => 'admin'], function(){
   Route::get('news/create',
   'Admin\NewsController@add');
 });
+
+Route::get('profile/create',
+'Admin\ProfileController@add');
+
+Route::get('profile/edit'
+'Admin\ProfileController@edit');
